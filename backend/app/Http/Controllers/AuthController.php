71d430ca91999cf8e5bcf192a3955a266a800a0c
João Vitor <61944386+Drack112@users.noreply.github.com>
2023-02-10 16:31:45 -0300

    :star2: refactor requests folder

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\RegisterRequest;
+use App\Http\Requests\Auth\RegisterRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
