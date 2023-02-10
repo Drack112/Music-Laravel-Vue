10e6ec9a39dee3a43530a45aa2c4ce1fa239df6e
João Vitor <61944386+Drack112@users.noreply.github.com>
2023-02-09 22:33:31 -0300

    :headphones: Compatibility with APlayer Lib

---
+++ frontend/src/components/partials/profile/SongPlayer.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="bg-green-500 rounded">
+    <div id="aplayer"></div>
+  </div>
+</template>
+
+<script setup>
+import { onMounted } from "vue";
+import Aplayer from "aplayer";
+
+import "aplayer/dist/APlayer.min.css";
+
+onMounted(() => {
+  thePlayer();
+});
+
+const thePlayer = () => {
+  new Aplayer({
+    container: document.getElementById("aplayer"),
+    audio: [],
+  });
+};
+</script>
