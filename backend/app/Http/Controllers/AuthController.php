fa0985012ec9d7091023d99de8fd8be7f2523f61
João Vitor <61944386+Drack112@users.noreply.github.com>
2023-02-10 15:00:25 -0300

    :mouse: Controller and Route to auth controller

---
+++ backend/app/Http/Controllers/AuthController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        try {
+            $user = User::create([
+                'first_name' => $request->input("first_name"),
+                'last_name' => $request->input("last_name"),
+                'email' => $request->input("email"),
+                'password' => Hash::make($request->input("password"))
+            ]);
+
+            $token = $user->createToken("user_token")->plainTextToken;
+
+            return response()->json([
+                'user' => $user,
+                'token' => $token,
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                "error" => $e->getMessage(),
+                "message" => "Something went wrong in AuthController.register"
+            ], 400);
+        }
+        ;
+    }
+
+    public function login(Request $request)
+    {
+        try {
+            $user = User::where("email", "=", $request->input("email"))->firstOrFail();
+
+            if (Hash::check($request->input("password"), $user->password)) {
+                $token = $user->createToken("user_token")->plainTextToken;
+                return response()->json([
+                    "user" => $user,
+                    "token" => $token,
+                ]);
+            }
+
+            return response()->json([
+                "error" => "Your password does not match!",
+            ], 403);
+
+
+        } catch (\Exception $e) {
+            return response()->json([
+                "error" => $e->getMessage(),
+                "message" => "Something went wrong in AuthController.login"
+            ], 400);
+        }
+    }
+
+    public function logout(Request $request)
+    {
+        try {
+            $user = User::findOrFail($request->input("user_id"));
+
+            $user->tokens()->delete();
+
+            return response()->json("User logged out", 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                "error" => $e->getMessage(),
+                "message" => "Something went wrong in AuthController.logout"
+            ], 400);
+        }
+    }
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,10 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get("/api/hello", function () {
-    return response("This is working!", 200);
-});
+Route::post("/register", [AuthController::class, 'register']);
+Route::post("/login", [AuthController::class, 'login']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post("logout", [AuthController::class, 'logout']);
 });
