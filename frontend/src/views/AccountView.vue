f87f7fe0a617ee06617f95ac9499151e915b72ef
João Vitor <61944386+Drack112@users.noreply.github.com>
2023-02-10 11:32:48 -0300

    :art: add min-heighton account view

--- frontend/src/views/AccountView.vue
+++ frontend/src/views/AccountView.vue
@@ -6,6 +6,12 @@
   <FooterSection />
 </template>
 
+<style lang="scss">
+#AccountView {
+  min-height: 70vh;
+}
+</style>
+
 <script setup>
 import TopNavigation from "@/components/structure/TopNavigation.vue";
 import FooterSection from "@/components/structure/ FooterSection.vue";
