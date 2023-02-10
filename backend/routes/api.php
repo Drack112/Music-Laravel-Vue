0ae55e4b3f824711e1d596efb3fb93efaa2611e7
João Vitor <61944386+Drack112@users.noreply.github.com>
2023-02-10 16:02:04 -0300

    :snake: fix routes and remove useless router of welcome

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
@@ -15,8 +14,8 @@ use App\Http\Controllers\AuthController;
 |
 */
 
-Route::post("/register", [AuthController::class, 'register']);
-Route::post("/login", [AuthController::class, 'login']);
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post("logout", [AuthController::class, 'logout']);
