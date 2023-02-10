39978ce20ba60a1193e5dd065eac0c266fde7227
João Vitor <61944386+Drack112@users.noreply.github.com>
2023-02-09 23:23:56 -0300

    :gem: put footer component on AccountView

--- frontend/src/views/AccountView.vue
+++ frontend/src/views/AccountView.vue
@@ -3,8 +3,10 @@
   <div id="AccountView" class="max-w-4xl mx-auto">
     <router-view />
   </div>
+  <FooterSection />
 </template>
 
 <script setup>
 import TopNavigation from "@/components/structure/TopNavigation.vue";
+import FooterSection from "@/components/structure/ FooterSection.vue";
 </script>
