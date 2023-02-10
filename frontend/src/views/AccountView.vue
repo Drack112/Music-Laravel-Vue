88fff5973d2dc4a576dfd99c1b51b549e698e430
João Vitor <61944386+Drack112@users.noreply.github.com>
2023-02-09 21:41:11 -0300

    :bee: Account Section with Profile Partials

---
+++ frontend/src/views/AccountView.vue
@@ -0,0 +1,7 @@
+<template>
+  <div id="AccountView">
+    <router-view />
+  </div>
+</template>
+
+<script setup></script>
---
+++ frontend/src/views/account/ProfileSection.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container max-w-4xl mx-auto flex">
+    <div class="w-1/3">
+      <img
+        class="w-full rounded-lg h-auto shadow-lg"
+        src="https://via.placeholder/500"
+        alt="Profile Pic"
+      />
+    </div>
+    <div class="w-full pl-4">
+      <div class="flex">
+        <div class="w-1/2">
+          <h1 class="text-2xl md:text-4xl text-left text-gray-900">John Doe</h1>
+          <span class="text-md text-gray-700">
+            <i><b>London, UK</b></i>
+          </span>
+        </div>
+
+        <div class="w-1/2 mt-2">
+          <router-link
+            class="float-right bg-transparent text-gray-900 hover:text-white py-2 px-4 border hover:border-transparent rounded hover:bg-green-500 border-green-500"
+            to=""
+            >Edit Profile</router-link
+          >
+        </div>
+      </div>
+      <ProfileInfoSection />
+      <ProfileAboutSection />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import ProfileInfoSection from "@/components/partials/profile/ProfileInfoSection.vue";
+import ProfileAboutSection from "@/components/partials/profile/ProfileAboutSection.vue";
+</script>
