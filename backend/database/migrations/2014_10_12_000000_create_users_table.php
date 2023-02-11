cbb8191f57052e2d959d2fad163a9ad9682b8991
João Vitor <61944386+Drack112@users.noreply.github.com>
2023-02-11 13:24:46 -0300

    :ng_woman: update user table

--- backend/database/migrations/2014_10_12_000000_create_users_table.php
+++ backend/database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,9 @@ return new class extends Migration {
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->text("image")->nullable();
+            $table->text("location")->nullable();
+            $table->text("description")->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
