3096dec72281159ff4638dbfbe1370872ecc65f7
João Vitor <61944386+Drack112@users.noreply.github.com>
2023-02-11 13:25:12 -0300

    :hammer: create service to upload image on local path with laravel

---
+++ backend/app/Services/ImageService.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Services;
+
+use ImageIntervention;
+
+class ImageService
+{
+    public function updateImage($model, $request, $path, $methodType)
+    {
+        $image = ImageIntervention::make($request->file("image"));
+
+        if (!empty($model->image)) {
+            $currentImage = public_path() . $path . $model->image;
+
+            if (file_exists($currentImage)) {
+                unlink($currentImage);
+            }
+        }
+
+        $file = $request->file('image');
+        $extension = $file->getClientOriginalExtension();
+
+        $image->crop(
+            $request->width,
+            $request->height,
+            $request->top,
+            $request->left,
+        );
+
+        $name = time() . '.' . $extension;
+        $image->save(public_path() . $path . $name);
+
+        if ($methodType === 'store') {
+            $model->user_id = $request->get('user_id');
+        }
+
+        $model->image = $name;
+
+        $model->save();
+
+    }
+}
+?>
