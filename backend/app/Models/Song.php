81f0013a06edcbca02effd8027e27cd1efdb4fd1
João Vitor <61944386+Drack112@users.noreply.github.com>
2023-02-13 12:27:14 -0300

    :gem: update model song

--- backend/app/Models/Song.php
+++ backend/app/Models/Song.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Song extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        "user_id",
+        "title",
+        "song"
+    ];
 }
