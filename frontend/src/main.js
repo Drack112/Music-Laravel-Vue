78e28f1727ba5af5ca890af90efe9f0b8846a9cd
João Vitor <61944386+Drack112@users.noreply.github.com>
2023-02-13 12:29:46 -0300

    :rocket: setup router with axios

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,9 +1,18 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-
 import "./registerServiceWorker";
 import router from "./router";
+import "./axios.js";
 
-import "./styles/tailwind.css";
+import { createPinia } from "pinia";
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 
-createApp(App).use(router).mount("#app");
+const pinia = createPinia();
+pinia.use(piniaPluginPersistedstate);
+
+const app = createApp(App);
+
+app.use(router);
+app.use(pinia);
+
+app.mount("#app");
