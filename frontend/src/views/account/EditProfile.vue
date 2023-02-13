fda28e15768747bd801274840bf51f9356309fd1
João Vitor <61944386+Drack112@users.noreply.github.com>
2023-02-13 12:29:35 -0300

    :capricorn: profile edit with axios

--- frontend/src/views/account/EditProfile.vue
+++ frontend/src/views/account/EditProfile.vue
@@ -71,28 +71,75 @@
 
     <div class="flex flex-wrap mt-8 mb-6">
       <div class="w-full px-3">
-        <SubmitFormButton btnText="Update Profile" />
+        <SubmitFormButton btnText="Update Profile" @click="updateUser" />
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { onMounted, ref } from "vue";
+import { useRouter } from "vue-router";
+import { useUserStore } from "../../store/user-store";
 import CroppedImage from "../../components/global/CroppedImage.vue";
 import TextInput from "../../components/global/TextInput.vue";
 import TextArea from "../../components/global/TextArea.vue";
 import CropperModal from "../../components/global/CropperModal.vue";
 import SubmitFormButton from "../../components/global/SubmitFormButton.vue";
 import DisplayCropperButton from "../../components/global/DisplayCropperButton.vue";
+import axios from "axios";
+
+const router = useRouter();
+const userStore = useUserStore();
+
 let showModal = ref(false);
 let firstName = ref(null);
 let lastName = ref(null);
 let location = ref(null);
 let description = ref(null);
+let imageData = null;
 let image = ref(null);
 let errors = ref([]);
+
+onMounted(() => {
+  firstName.value = userStore.firstName || null;
+  lastName.value = userStore.lastName || null;
+  location.value = userStore.location || null;
+  description.value = userStore.description || null;
+  image.value = userStore.image || null;
+});
+
 const setCroppedImageData = (data) => {
+  imageData = data;
   image.value = data.imageUrl;
+};
+
+const updateUser = async () => {
+  errors.value = [];
+
+  let data = new FormData();
+
+  data.append("first_name", firstName.value || "");
+  data.append("last_name", lastName.value || "");
+  data.append("location", location.value || "");
+  data.append("description", description.value || "");
+
+  if (imageData) {
+    data.append("image", imageData.file || "");
+    data.append("height", imageData.height || "");
+    data.append("width", imageData.width || "");
+    data.append("left", imageData.left || "");
+    data.append("top", imageData.top || "");
+  }
+
+  try {
+    await axios.post("api/user/" + userStore.id + "?_method=PUT", data);
+
+    await userStore.fetchUser();
+
+    router.push("/account/profile/" + userStore.id);
+  } catch (err) {
+    errors.value = err.response.data.errors;
+  }
 };
 </script>
