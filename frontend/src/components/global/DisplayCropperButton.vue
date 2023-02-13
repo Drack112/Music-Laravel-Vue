533213e36f79a8873ddacc64b0e5c5c8b99c6e68
João Vitor <61944386+Drack112@users.noreply.github.com>
2023-02-13 12:32:36 -0300

    :light_rail: update some components

--- frontend/src/components/global/DisplayCropperButton.vue
+++ frontend/src/components/global/DisplayCropperButton.vue
@@ -12,6 +12,7 @@
 
 <script setup>
 import { defineProps, toRefs } from "vue";
+
 const props = defineProps({ btnText: String, label: String });
 const { btnText, label } = toRefs(props);
 </script>
