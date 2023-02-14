388e409acc86870647776ef575deff4751aab453
João Vitor <61944386+Drack112@users.noreply.github.com>
2023-02-14 15:55:52 -0300

    :snake: fix api routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\SongByUserController;
 use App\Http\Controllers\SongController;
 use App\Http\Controllers\UserController;
@@ -28,11 +29,18 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::put("/user/{id}", [UserController::class, "update"]);
 
     Route::post("songs", [SongController::class, "store"]);
-    Route::delete("songs/{id}/{user_id}/", [SongController::class], "destroy");
+    Route::delete("songs/{id}/{user_id}/", [SongController::class, "destroy"]);
 
-    Route::get("user/{user_id}/songs", [SongByUserController::class], "index");
+    Route::get("user/{user_id}/songs", [SongByUserController::class, "index"]);
 
-    Route::get('youtube/{user_id}', [YoutubeController::class], 'show');
-    Route::post('youtube', [YoutubeController::class], 'store');
-    Route::delete('youtube/{id}', [YoutubeController::class], 'destroy');
+    Route::get('youtube/{user_id}', [YoutubeController::class, 'show']);
+    Route::post('youtube', [YoutubeController::class, 'store']);
+    Route::delete('youtube/{id}', [YoutubeController::class, 'destroy']);
+
+    Route::get('posts', [PostController::class, 'index']);
+    Route::get('posts/{id}', [PostController::class, 'show']);
+    Route::post('posts', [PostController::class,  'store']);
+    Route::put('posts/{id}', [PostController::class, 'update']);
+    Route::delete('posts/{id}', [PostController::class, 'destroy']);
+
 });
